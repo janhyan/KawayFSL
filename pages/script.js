35b97af0305f5fcae7e52c2b44d32bad1ada5704
Hyan <hyanxjan@gmail.com>
2024-08-11 11:32:54 -0400

    Added side-nav footer animation.

--- pages/script.js
+++ pages/script.js
@@ -1,14 +1,39 @@
 /* SIDE NAV */
 
-function slideFooter() {
-    var footerToggle = document.getElementById("footer-caret");
-    var footer = document.querySelector(".side-nav.footer");
 
-    console.log('test')
-    if (footerToggle.checked == true) {
-        console.log("Test")
-        footer.style.bottom = '0%';
+/* Function to slide the side-nav footer */
+function slideFooter() {
+  const footerToggle = document.getElementById("footer-caret");
+  const footer = document.querySelector(".side-nav.footer");
+
+  let id = null;
+  let initialUp = -60;
+  let initialDown = 0;
+  clearInterval(id);
+
+  if (footerToggle.checked == true) {
+    id = setInterval(slideUp, 10);
+  } else {
+    id = setInterval(slideDown, 10);
+  }
+
+  function slideUp() {
+
+    if (initialUp == 0) {
+      clearInterval(id);
     } else {
-        footer.style.bottom = '-60%';
+      initialUp++;
+      footer.style.bottom = initialUp + "%";
     }
+  }
+
+  function slideDown() {
+
+    if (initialDown == -60) {
+      clearInterval(id);
+    } else {
+      initialDown--;
+      footer.style.bottom = initialDown + "%";
+    }
+  }
 }

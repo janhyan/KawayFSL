6b4c4cb596b8e553a0d23a1b208755e8cb7b98ba
Hyan <hyanxjan@gmail.com>
2024-08-17 03:16:02 -0400

    Feat: Transitioned Home page to React

---
+++ kawayfsl/src/data/user.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id: 1,
+        username: 'Julian',
+        age: '22',
+        grade: '10',
+        section: 'Socrates',
+    },
+    {
+        id: 2,
+        username: 'Josh',
+        age: '21',
+        grade: '9',
+        section: 'Aristotle',
+    },
+    {
+        id: 3,
+        username: 'Kayley',
+        age: '23',
+        grade: '11',
+        section: 'Plato',
+    }
+]

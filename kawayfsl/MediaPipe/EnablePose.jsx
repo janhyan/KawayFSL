658ce5ff82e113dd307e8c21f3c334c79fb275af
hyanjan <hyanxjan@gmail.com>
2024-08-22 13:35:09 +0800

    Reverting to commit (3e25d7a)

--- kawayfsl/MediaPipe/EnablePose.jsx
+++ kawayfsl/MediaPipe/EnablePose.jsx
@@ -4,28 +4,14 @@ import {
   DrawingUtils,
 } from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.10.0";
 
-let poseLandmarker = undefined;
+
 let runningMode = "IMAGE";
 let enableWebcamButton = HTMLButtonElement;
 let webcamRunning = false;
-const videoHeight = "360px";
-const videoWidth = "480px";
+
 
 
 // Before we can use PoseLandmarker class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-export const createPoseLandmarker = async () => {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
-    );
-    poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
-      baseOptions: {
-        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
-        delegate: "GPU"
-      },
-      runningMode: runningMode,
-      numPoses: 2
-    });
-  };
+
   createPoseLandmarker();

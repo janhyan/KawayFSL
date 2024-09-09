9cf0957ab940cbd172d7b79f29a74342c873372a
hyanjan <hyanxjan@gmail.com>
2024-09-09 16:01:04 +0800

    refactor: Configured database connections with the backend for local testin

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
-const pgp = require("pg-promise");
-const db = pgp(process.env.DATABASE)
+require("dotenv").config();
+
+const pgp = require("pg-promise")();
+const connection = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+const db = pgp(connection);
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-    return res.send("Hello from Home Page");
-})
+  db.any("SELECT * FROM Lessons")
+    .then((data) => {
+      return res.send(data);
+    })
+    .catch((err) => {
+      return res.send(err);
+    });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});

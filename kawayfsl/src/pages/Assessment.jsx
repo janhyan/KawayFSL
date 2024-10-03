161367054ce1b9638126601ef6733fec88b944b1
hyanjan <hyanxjan@gmail.com>
2024-10-03 21:22:44 +0800

    feat: Change button to allow user to go to next lesson if sign is correct

--- kawayfsl/src/pages/Assessment.jsx
+++ kawayfsl/src/pages/Assessment.jsx
@@ -14,6 +14,7 @@ export default function Assessment() {
 
   // Set answer and counter states
   const [answer, setAnswers] = React.useState([]);
+  const currentAnswer = React.useRef();
   const [counter, setCounter] = React.useState(3);
 
   // Set states and ref for triggering certain functions
@@ -22,6 +23,10 @@ export default function Assessment() {
   const [isCount, setIsCount] = React.useState(false); // For triggering countdown when tracking starts
   const counterRef = React.useRef(counter);
   const isCounterRef = React.useRef(false);
+
+  answer.map((currentUserAnswer) => {
+    currentAnswer.current = currentUserAnswer;
+  })
 
   // Countdown logic for state updates
   React.useEffect(() => {
@@ -67,8 +72,6 @@ export default function Assessment() {
   }
 
   function toggleRecord() {
-    console.log(contentData.answers)
-
     setIsCount((prevCount) => !prevCount);
     isCounterRef.current = true;
 
@@ -88,6 +91,7 @@ export default function Assessment() {
         subtopic={contentData.lesson_title}
         dbAnswer={contentData.answers}
         counter={counter}
+        currentUserAnswer={currentAnswer.current}
       />
     </div>
   );
@@ -109,14 +113,17 @@ function MainBody(props) {
             <button className="enable_fsl" onClick={props.enable}>
               CAMERA
             </button>
-            <button className="record" onClick={props.toggle}>
-              START
-            </button>
+            <UserButton userAnswer={props.currentUserAnswer} dbAnswer={props.dbAnswer} toggle={props.toggle} />
           </div>
         </div>
         <div className="right-body">
           {props.answers.map((answer, index) => (
-            <Answers userAnswer={answer} dbAnswer={props.dbAnswer} key={index} answer={answer} />
+            <Answers
+              userAnswer={answer}
+              dbAnswer={props.dbAnswer}
+              key={index}
+              answer={answer}
+            />
           ))}
         </div>
       </div>
@@ -126,31 +133,51 @@ function MainBody(props) {
 
 // Render divs for answers
 function Answers(props) {
-  return <div className="answer" style={checkResult(props.userAnswer, props.dbAnswer) ? {backgroundColor: "#00CC00"} : {backgroundColor: "#CC0000"} }>{props.answer}</div>;
+  return (
+    <div
+      className="answer"
+      style={
+        checkResult(props.userAnswer, props.dbAnswer)
+          ? { backgroundColor: "#00CC00" }
+          : { backgroundColor: "#CC0000" }
+      }
+    >
+      {props.answer}
+    </div>
+  );
 }
 
 // Check if result is correct
 function checkResult(userAnswer, dbAnswer) {
-  console.log(userAnswer, dbAnswer);
   if (userAnswer == dbAnswer) {
-    console.log("Correct");
     return true;
   } else {
     return false;
   }
 }
 
-function ResultButton(props) {
-  return (
-    <div className="button-container">
-      <Button
-        className={props.btnName}
-        as={Link}
-        to={props.page}
-        state={props.state}
-      >
-        {props.btnText}
-      </Button>
-    </div>
-  );
+function UserButton(props) {
+  if (checkResult(props.userAnswer, props.dbAnswer)) {
+    return (
+      <div className="button-container">
+        <Button
+          className="next-lesson"
+          as={Link}
+          to={props.page}
+        >
+          Next Lesson
+        </Button>
+      </div>
+    );
+  } else {
+    return (
+      <button className="record" onClick={props.toggle}>
+        START
+      </button>
+    );
+  }
 }
+
+// function getNextLesson(props) {
+
+// }

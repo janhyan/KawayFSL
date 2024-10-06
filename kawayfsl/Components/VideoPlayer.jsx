e39847069fe78b5e962a9bea8c48d527d2a5d3a3
hyanjan <hyanxjan@gmail.com>
2024-10-06 23:32:02 +0800

    VideoPlayer component added to load lesson videos with react-player

---
+++ kawayfsl/Components/VideoPlayer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+export default function VideoPlayer() {
+  return (
+    <ReactPlayer
+      url="<https://www.youtube.com/watch?v=ysz5S6PUM-U>"
+      width="640"
+      height="360"
+      controls
+    />
+  );
+}

8f4404a7592a35276aceb1a326403b2f413aad65
hyanjan <hyanxjan@gmail.com>
2024-10-09 21:48:51 +0800

    refactor: Changed dockerfile according to best practices

--- kawayfsl/Components/VideoPlayer.jsx
+++ kawayfsl/Components/VideoPlayer.jsx
@@ -1,21 +1,57 @@
 import ReactPlayer from "react-player/lazy";
 
 export default function VideoPlayer(props) {
-  return (
-    <ReactPlayer
-      url="<https://d3soyatq5ls79q.cloudfront.net/module-one/A.mp4>"
-      config={{
-        file: {
-          attributes: {
-            crossOrigin: "anonymous",
-            headers: { Authorization: `Bearer ${props.token}` },
+    const accessToken = props.token; // Retrieve your access token
+    const path = 'https://d3soyatq5ls79q.cloudfront.net/module-one/C.mp4';
+  
+    return (
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        url={path}
+        config={{
+          file: {
+            forceHLS: true,
+            hlsOptions: {
+              xhrSetup: function (xhr, url) {
+                xhr.open("GET", url, true);
+                xhr.setRequestHeader("Authorization", `Bearer ${accessToken}`);
+              },
+            },
           },
-        },
-      }}
-      //   width="640"
-      //   height="360"
-      controls
-      // url={`https://${props.module}/${props.lesson}`}
-    />
-  );
+        }}
+        controls
+      />
+    );
+
+  // return (
+  //   <ReactPlayer
+  //     url="<https://d3soyatq5ls79q.cloudfront.net/module-one/B.mp4>"
+  //     // config={{
+  //     //   file: {
+  //     //     attributes: {
+  //     //       crossOrigin: "anonymous",
+  //     //       headers: { Authorization: `Bearer ${props.token}` },
+  //     //     },
+  //     //   },
+  //     // }}
+  //     config={{
+  //       file: {
+  //         hlsOptions: {
+  //           forceHLS: true,
+  //           debug: false,
+  //           xhrSetup: function(xhr, url) {
+  //             if (needsAuth(url)) {
+  //               xhr.setRequestHeader('Authorization', getToken())
+  //             }
+  //           },
+  //         },
+  //       },
+  //     }}
+  //     //   width="640"
+  //     //   height="360"
+  //     controls
+  //     // url={`https://${props.module}/${props.lesson}`}
+  //   />
+  // );
 }

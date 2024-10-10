8896b8e1ed1310195c0cf3a67b56513571ff952c
hyanjan <hyanxjan@gmail.com>
2024-10-10 23:06:09 +0800

    HLS option for streaming video added

--- kawayfsl/Components/VideoPlayer.jsx
+++ kawayfsl/Components/VideoPlayer.jsx
@@ -2,34 +2,63 @@ import ReactPlayer from "react-player/lazy";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+// export default function VideoPlayer(props) {
+//   const [videoUrl, setVideoUrl] = useState(null);
+//   const accessToken = props.token; // Retrieve your access token
+
+//   useEffect(() => {
+//     const fetchVideo = async () => {
+//       try {
+//         const response = await axios.get("https://d3soyatq5ls79q.cloudfront.net/3/Countdown_1Day.m3u8", {
+//           headers: { Authorization: `Bearer ${accessToken}` },
+//           responseType: 'blob',
+//         });
+
+//         const blobUrl = URL.createObjectURL(response.data);
+//         setVideoUrl(blobUrl); // Set the video URL from the blob
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     };
+
+//     fetchVideo();
+//   }, [accessToken]);
+
+//   return (
+//     <ReactPlayer
+//       width="100%"
+//       height="100%"
+//       url={videoUrl} // Use the fetched blob URL
+//       controls
+//     />
+//   );
+// }
+
+// HLS STREAMING OPTION
 export default function VideoPlayer(props) {
-  const [videoUrl, setVideoUrl] = useState(null);
   const accessToken = props.token; // Retrieve your access token
-
-  useEffect(() => {
-    const fetchVideo = async () => {
-      try {
-        const response = await axios.get("https://d3soyatq5ls79q.cloudfront.net/module-one/H.mp4", {
-          headers: { Authorization: `Bearer ${accessToken}` },
-          responseType: 'blob',
-        });
-
-        const blobUrl = URL.createObjectURL(response.data);
-        setVideoUrl(blobUrl); // Set the video URL from the blob
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    fetchVideo();
-  }, [accessToken]);
+  const videoUrl = "https://d3soyatq5ls79q.cloudfront.net/1/Countdown_1Day.m3u8"; // HLS URL
 
   return (
     <ReactPlayer
+      url={videoUrl}
       width="100%"
       height="100%"
-      url={videoUrl} // Use the fetched blob URL
       controls
+      config={{
+        file: {
+          attributes: {
+            crossOrigin: "anonymous", // Enable CORS
+          },
+          forceHLS: true,
+          hlsOptions: {
+            xhrSetup: function (xhr) {
+              xhr.setRequestHeader("Authorization", `Bearer ${accessToken}`); // Set authorization header
+            },
+          },
+        },
+      }}
     />
   );
 }
+

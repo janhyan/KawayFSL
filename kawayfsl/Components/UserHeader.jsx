50c429015a3d4f4f5c257a24256a3f3d762461f5
hyanjan <hyanxjan@gmail.com>
2024-10-20 23:53:05 +0800

    feat: Notif button returns data from API

--- kawayfsl/Components/UserHeader.jsx
+++ kawayfsl/Components/UserHeader.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default function UserHeader(props) {
     return (
       <header className="title-header">
@@ -8,8 +10,23 @@ export default function UserHeader(props) {
           <h1>{props.greetings}</h1>
         </div>
         <div className="notif-container">
-          <button className="notif-button" />
+          <button className="notif-button" onClick={} />
         </div>
       </header>
     );
   }
+
+
+function getNotif(user) {
+  if (user) {
+    return axios
+    // .get("https://alb.kawayfsl.com/v1/notifications", {params: {user: user?.sub}})
+    .get("http://localhost:8080/v1/notifications", {params: {user: user?.sub}})
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+} 

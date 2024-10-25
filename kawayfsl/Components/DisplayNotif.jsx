28cd554d62fe1a0ab34a956c26e807f00d037cae
hyanjan <hyanxjan@gmail.com>
2024-10-25 23:01:26 +0800

    feat: Added checkbox feature to to-do list on home page

--- kawayfsl/Components/DisplayNotif.jsx
+++ kawayfsl/Components/DisplayNotif.jsx
@@ -1,117 +1,118 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function DisplayNotif(props) {
-    const [notifications, setNotifications] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    const fetchNotifications = async () => {
-      try {
-        const notif = await getNotif(props.user);
-        console.log("notif", notif);
-        setNotifications(notif);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching notifications:", error);
-        setLoading(false);
-      }
-    };
-  
-    useEffect(() => {
-      fetchNotifications();
-    }, [props.user]);
-  
-    if (loading) {
-      return (
-        <div className="notif-menu-container">
-          <l-quantum size="50" speed="1.75" color="#219ebc"></l-quantum>
-          <button
-            id="close-popup"
-            onClick={(e) => {
-              e.preventDefault();
-              location.href = "#";
-            }}
-          >
-            &times;
-          </button>
-        </div>
-      );
+  const [notifications, setNotifications] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchNotifications = async () => {
+    try {
+      const notif = await getNotif(props.user);
+      console.log("notif", notif);
+      setNotifications(notif);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching notifications:", error);
+      setLoading(false);
     }
-  
-    if (!loading) {
-      console.log(notifications);
-      return (
-        <div className="notif-menu-container">
-          <button
-            id="close-popup"
-            onClick={(e) => {
-              e.preventDefault();
-              location.href = "#";
-            }}
-          >
-            &times;
-          </button>
-          {notifications?.map((notifMsg, i, arr) => (
-            <div className="notif-content" key={notifMsg.notification_id}>
-              <p>{notifMsg.notif_message}</p>
-              <button
-                className="close"
-                onClick={() => {
-                  updateNotif(
-                    props.user,
-                    notifMsg.notification_id,
-                    fetchNotifications
-                  );
-                  setLoading(true);
-                }}
-              >
-                &#10003;
-              </button>
-              {!(i + 1 === arr.length) ? <hr className="break" /> : null}
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      return (
-        <div className="notif-menu-container">
-          <div className="notif-content">
-            <p>No notifications available.</p>
+  };
+
+  useEffect(() => {
+    fetchNotifications();
+  }, [props.user]);
+
+  if (loading) {
+    return (
+      <div className="notif-menu-container">
+        <l-quantum size="50" speed="1.75" color="#219ebc"></l-quantum>
+        <button
+          id="close-popup"
+          onClick={(e) => {
+            e.preventDefault();
+            location.href = "#";
+          }}
+        >
+          &times;
+        </button>
+      </div>
+    );
+  }
+
+  if (!loading) {
+    console.log(notifications);
+    return (
+      <div className="notif-menu-container">
+        <button
+          id="close-popup"
+          onClick={(e) => {
+            e.preventDefault();
+            location.href = "#";
+          }}
+        >
+          &times;
+        </button>
+        {notifications?.map((notifMsg, i, arr) => (
+          <div className="notif-content" key={notifMsg.notification_id}>
+            <p>{notifMsg.notif_message}</p>
+            <button
+              className="close"
+              onClick={() => {
+                updateNotif(
+                  props.user,
+                  notifMsg.notification_id,
+                  fetchNotifications
+                );
+                setLoading(true);
+              }}
+            >
+              &#10003;
+            </button>
+            {!(i + 1 === arr.length) ? <hr className="break" /> : null}
           </div>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className="notif-menu-container">
+        <div className="notif-content">
+          <p>No notifications available.</p>
         </div>
+      </div>
+    );
+  }
+}
+
+async function getNotif(user) {
+  if (user) {
+    try {
+      const response = await axios.get(
+        "http://localhost:6868/v1/notifications",
+        {
+          params: { user: user.sub },
+        }
       );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      return [];
     }
   }
-  
-  async function getNotif(user) {
-    if (user) {
-      try {
-        const response = await axios.get(
-          "http://localhost:6868/v1/notifications",
-          {
-            params: { user: user.sub },
-          }
-        );
-        return response.data;
-      } catch (error) {
+  return [];
+}
+
+function updateNotif(user, notification_id, fetchNotifications) {
+  if (user.sub) {
+    axios
+      .patch(`http://localhost:6868/v1/notifications/${notification_id}`, {
+        user: user.sub,
+      })
+      .then((response) => {
+        console.log(response.data);
+        fetchNotifications();
+      })
+      .catch((error) => {
         console.error(error);
-        return [];
-      }
-    }
-    return [];
+      });
   }
-  
-  function updateNotif(user, notification_id, fetchNotifications) {
-    if (user.sub) {
-      axios
-        .patch(`http://localhost:6868/v1/notifications/${notification_id}`, {
-          user: user.sub,
-        })
-        .then((response) => {
-          console.log(response.data);
-          fetchNotifications();
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }
+}
--- kawayfsl/Components/ToDoCard.jsx
+++ kawayfsl/Components/ToDoCard.jsx
@@ -1,11 +1,42 @@
+import { useState, useEffect } from "react";
+
 export default function ToDoCard() {
-    return (
-      <div className="todo card">
-        <h2 className="todo-title">To-do</h2>
-        <ul>
-          <li>No items to display</li>
-          <li>No items to display</li>
-        </ul>
-      </div>
-    );
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      msg: "Test Message",
+      status: false,
+    },
+  ]);
+
+  return (
+    <div className="todo card">
+      <h2 className="todo-title">To-do</h2>
+      <Task tasks={tasks} setTasks={setTasks} />
+    </div>
+  );
+}
+
+function Task(props) {
+    const checklist = props.tasks.map((task) => {
+      return (
+        <label className="container" key={task.id}>
+          {task.msg}
+          <input
+            type="checkbox"
+            checked={task.status}
+            onChange={() =>
+              props.setTasks((prevTasks) =>
+                prevTasks.map((t) =>
+                  t.id === task.id ? { ...t, status: !t.status } : t
+                )
+              )
+            }
+          />
+          <span className="checkmark"></span>
+        </label>
+      );
+    });
+  
+    return <div>{checklist}</div>;
   }

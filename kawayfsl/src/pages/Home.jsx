692fd4d5982870bc3acff1bafb6c58b1172745d4
hyanjan <hyanxjan@gmail.com>
2024-11-17 23:15:46 +0800

    fix: Ping detection API on Home page as a temporary approach to keeping lambdas warm

--- kawayfsl/src/pages/Home.jsx
+++ kawayfsl/src/pages/Home.jsx
@@ -5,9 +5,13 @@ import Navbar from "../../Components/Navbar.jsx";
 import HomeContent from "../../Components/HomeContent.jsx";
 import { AuthContext } from "../auth/authContext.jsx";
 import { useContext } from "react";
+import axios from "axios"
 
 export default function Home() {
   const { user } = useContext(AuthContext);
+  
+  pingStatic([0,1,2]);
+  pingHolistic([0, 1, 2], 1);
 
   if (!user) {
     return <Navigate to="/signin" />;
@@ -20,3 +24,32 @@ export default function Home() {
     );
   }
 }
+
+function pingStatic(sequence) {
+  axios
+    .post(
+      "https://kb02bv2ra8.execute-api.ap-northeast-1.amazonaws.com/stage/static",
+      sequence
+    )
+    .then((response) => {
+      setAnswers((prevAnswer) => [...prevAnswer, response.data]);
+    })
+    .catch((error) => {
+      console.error("Error sending sequence to API:", error);
+    });
+}
+
+function pingHolistic(sequence, assessment_id) {
+  axios
+    .post(
+      "https://kb02bv2ra8.execute-api.ap-northeast-1.amazonaws.com/stage/",
+      { sequence, assessment_id } // Send both sequence and assessment_id in the body
+    )
+    .then((response) => {
+      setAnswers((prevAnswer) => [...prevAnswer, response.data]);
+    })
+    .catch((error) => {
+      console.error("Error sending sequence to API:", error);
+    });
+}
+
